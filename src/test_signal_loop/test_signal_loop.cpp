6be04fa142fc0b0de29bbe20ddcbcf7344a154fa
harris <hanpan_harris@163.com>
2021-09-14 22:19:08 +0800

    modify test_signal_loop.cpp

--- src/test_signal_loop/test_signal_loop.cpp
+++ src/test_signal_loop/test_signal_loop.cpp
@@ -1,26 +1,3 @@
-/*/*******************************************************************************
-**                                                                            **
-**                     Jiedi(China nanjing)Ltd.                               **
-**	               创建：丁宋涛 夏曹俊，此代码可用作为学习参考                **
-*******************************************************************************/
-
-/*****************************FILE INFOMATION***********************************
-**
-** Project       : Libevent C＋＋高并发网络编程
-** Contact       : xiacaojun@qq.com
-**  博客   : http://blog.csdn.net/jiedichina
-**	视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
-			   腾讯课堂		https://jiedi.ke.qq.com/				
-			   csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
-**             51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html	
-** 			   老夏课堂		http://www.laoxiaketang.com 
-**                 
-**  Libevent C＋＋高并发网络编程 课程群 ：1003847950 加入群下载代码和交流
-**   微信公众号  : jiedi2007
-**		头条号	 : 夏曹俊
-**
-*****************************************************************************
-//！！！！！！！！！ Libevent C＋＋高并发网络编程 课程  QQ群：1003847950 下载代码和交流*/
 #include <iostream>
 #include <event2/event.h>
 #include <signal.h>
@@ -37,7 +14,7 @@ static void Ctrl_C(int sock,short which,void *arg)
     //运行所有的活动事件再退出
     //事件循环没有运行时也要等运行一次再退出
     timeval t = {3,0}; //至少运行三秒后退出
-    event_base_lopexit(base,&t);
+    event_base_loopexit(base,&t);
 }
 
 static void Kill(int sock,short which,void *arg)
